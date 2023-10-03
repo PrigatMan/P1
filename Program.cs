29c847b1a5e9c757e5933040567bad9bc62311dd
PrigatMan <fieraru.andrei.1998@gmail.com>
2023-10-03 18:01:20 +0300

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Problema_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Introduceti numele: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Introduceti varsta: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine($"{name} are drept de vot.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} nu are drept de vot.");
+            }
+        }
+    }
+}
